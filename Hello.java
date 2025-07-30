f62eae57598294a6d1cc36a4efa02b23437fff6a
vijaysadh17 <vijay.sadh173@gmail.com>
2025-07-30 22:00:21 +0530

    Greeting function added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello
 {
+
+    public void greeting()
+        {
+            System.out.println("good morning");
+        }
+        
+    
     public static void main(String[] args) {
         System.out.println("hello vijay");
         System.out.println("hello Infosians");
